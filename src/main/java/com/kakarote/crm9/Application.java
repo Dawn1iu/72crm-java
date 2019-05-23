6aef89f378042cfdbba86f18f6f0f6e105852c4c
liujinquan <hzliujinquan@corp.netease.com>
2019-05-23 15:09:19 +0800

    @dev

--- src/main/java/com/kakarote/crm9/Application.java
+++ src/main/java/com/kakarote/crm9/Application.java
@@ -1,18 +1,14 @@
 package com.kakarote.crm9;
 
-import com.jfinal.server.undertow.UndertowServer;
-import com.kakarote.crm9.common.config.JfinalConfig;
-import com.kakarote.crm9.common.constant.BaseConstant;
-
 public class Application {
     public static void main(String[] args) {
         /**
          * jetty启动
          * JFinal.start("src/main/webapp", 80, "/", 5);
          */
-        UndertowServer
-                .create(JfinalConfig.class,"config/undertow.txt")
-                .setResourcePath("src/main/webapp,"+ BaseConstant.UPLOAD_PATH)
-                .start();
+//        UndertowServer
+//                .create(JfinalConfig.class,"config/undertow.txt")
+//                .setResourcePath("src/main/webapp,"+ BaseConstant.UPLOAD_PATH)
+//                .start();
     }
 }
